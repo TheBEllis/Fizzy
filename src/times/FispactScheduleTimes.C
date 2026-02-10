60d57ce57887b1c980c67bf2f759b5dcfee9d023
Bill Ellis <billellis.07@gmail.com>
2026-02-10 15:26:48 +0000

    Added options to FispactScheduleTimes to allow for only a subset of the times within the FispactSchedule to be passed to the executioner

--- src/times/FispactScheduleTimes.C
+++ src/times/FispactScheduleTimes.C
@@ -15,6 +15,13 @@ InputParameters FispactScheduleTimes::validParams() {
       "FispactSchedule",
       "Name of the FispactSchedule UserObject used to set times");
 
+  params.addParam<std::vector<int>>(
+      "FispactScheduleTimeIndices",
+      "The indices of the values within the schedule times the user wishes to "
+      "give to the executioner. This is useful if the user wishes to only "
+      "store data and/or run multiapps at a subset of the times defined within "
+      "the FispactSchedule");
+
   // Times are known for all processes already
   params.set<bool>("auto_broadcast") = false;
   params.set<bool>("dynamic_time_sequence") = false;
@@ -27,5 +34,18 @@ FispactScheduleTimes::FispactScheduleTimes(const InputParameters &parameters)
 
   const FispactSchedule &schedule =
       getUserObject<FispactSchedule>(getParam<std::string>("FispactSchedule"));
-  _times = schedule.getTimes();
+
+  if (isParamValid("FispactScheduleTimeIndices")) {
+    std::vector<int> indices =
+        getParam<std::vector<int>>("FispactScheduleTimeIndices");
+
+    std::vector<double> schedule_times = schedule.getTimes();
+    for (int index : indices) {
+      _times.push_back(schedule_times[index]);
+    }
+    _times = schedule.getTimes();
+  } else {
+
+    _times = schedule.getTimes();
+  }
 }
