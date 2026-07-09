e9f7c9c1e0b72bf8d3fa17d5a6b557928fe9766c
Bill Ellis <billellis.07@gmail.com>
2026-07-09 14:43:22 +0100

    Created stub library to stop errors when linking to normal fizzy app lib

---
+++ unit/stubs/fispact_stub.C
@@ -0,0 +1,115 @@
+extern "C" {
+
+void FispactElementalDataGetAtomicDisplacementEnergy() {}
+void FispactElementalDataGetNaturalIsotopeAbundances() {}
+void FispactElementalDataGetNrOfNaturalIsotopes() {}
+void FispactElementalDataGetRelativeAtomicMass() {}
+void FispactElementalDataGetStandardDensity() {}
+void FispactElementalDataReset() {}
+void FispactElementalDataSetAtomicDisplacementEnergy() {}
+void FispactElementalDataSetNaturalIsotopeAbundances() {}
+void FispactElementalDataSetRelativeAtomicMass() {}
+void FispactElementalDataSetStandardDensity() {}
+void FispactFinalise() {}
+void FispactGetAtomicNumberFromElementName() {}
+void FispactGetNuclideName() {}
+void FispactGetZai() {}
+void FispactGroupConvertByEnergy() {}
+void FispactGroupConvertByLethargy() {}
+void FispactInitialise() {}
+void FispactInputDataCreate() {}
+void FispactInputDataGetSchedule() {}
+void FispactInputDataGetScheduleNrOfEntries() {}
+void FispactInputDataRead() {}
+void FispactInputDataSetAtomsThreshold() {}
+void FispactInputDataSetDensity() {}
+void FispactInputDataSetFlux() {}
+void FispactInputDataSetFluxName() {}
+void FispactInputDataSetFluxWallLoading() {}
+void FispactInputDataSetFuel() {}
+void FispactInputDataSetMass() {}
+void FispactInputDataSetMassTotal() {}
+void FispactInputDataSetSchedule() {}
+void FispactInputDataSetSolverTolerance() {}
+void FispactInputDataWrite() {}
+void FispactNuclearDataCreate() {}
+void FispactNuclearDataDestroy() {}
+void FispactNuclearDataGetReactionXS() {}
+void FispactNuclearDataGetReactionXSNrOfBins() {}
+void FispactNuclearDataReaderCreate() {}
+void FispactNuclearDataReaderDestroy() {}
+void FispactNuclearDataReaderGetKeyName() {}
+void FispactNuclearDataReaderLoad() {}
+void FispactNuclearDataReaderRead() {}
+void FispactNuclearDataReaderSetPath() {}
+void FispactNuclearDataReaderSetUseXSBinary() {}
+void FispactNuclearDataReaderWrite() {}
+void FispactNuclearDataWriteBinary() {}
+void FispactOutputDataCreate() {}
+void FispactOutputDataFindInventoryNuclideExists() {}
+void FispactOutputDataFindInventoryNuclideIndex() {}
+void FispactOutputDataGetInventoryDoseRate() {}
+void FispactOutputDataGetInventoryGammaSpectrum() {}
+void FispactOutputDataGetInventoryGammaSpectrumNrOfBins() {}
+void FispactOutputDataGetInventoryNrOfNuclides() {}
+void FispactOutputDataGetInventoryNuclideAtIndex() {}
+void FispactOutputDataGetInventorySortedByKey() {}
+void FispactOutputDataGetInventoryValueByKey() {}
+void FispactOutputDataWrite() {}
+void FispactProcess() {}
+
+void MonitorCreate() {}
+void MonitorDestroy() {}
+void MonitorWriteToFile() {}
+
+int FISPACT_GROUP_100 = 0;
+int FISPACT_GROUP_1102 = 0;
+int FISPACT_GROUP_142 = 0;
+int FISPACT_GROUP_162 = 0;
+int FISPACT_GROUP_172 = 0;
+int FISPACT_GROUP_175 = 0;
+int FISPACT_GROUP_211 = 0;
+int FISPACT_GROUP_315 = 0;
+int FISPACT_GROUP_351 = 0;
+int FISPACT_GROUP_586 = 0;
+int FISPACT_GROUP_616 = 0;
+int FISPACT_GROUP_66 = 0;
+int FISPACT_GROUP_689 = 0;
+int FISPACT_GROUP_69 = 0;
+int FISPACT_GROUP_709 = 0;
+int FISPACT_MAX_KEY_LENGTH = 0;
+int FISPACT_ND_A2DATA_KEY = 0;
+int FISPACT_ND_ABSORP_KEY = 0;
+int FISPACT_ND_ASSCFY_KEY = 0;
+int FISPACT_ND_CLEAR_KEY = 0;
+int FISPACT_ND_CROSSEC_KEY = 0;
+int FISPACT_ND_CROSSUNC_KEY = 0;
+int FISPACT_ND_DECAY_KEY = 0;
+int FISPACT_ND_DK_ENDF_KEY = 0;
+int FISPACT_ND_ENBINS_KEY = 0;
+int FISPACT_ND_FISSYLD_KEY = 0;
+int FISPACT_ND_FY_ENDF_KEY = 0;
+int FISPACT_ND_HAZARDS_KEY = 0;
+int FISPACT_ND_IND_NUC_KEY = 0;
+int FISPACT_ND_PROB_TAB_KEY = 0;
+int FISPACT_ND_SF_ENDF_KEY = 0;
+int FISPACT_ND_SP_ENDF_KEY = 0;
+int FISPACT_ND_XS_ENDFB_KEY = 0;
+int FISPACT_ND_XS_ENDF_KEY = 0;
+int FISPACT_ND_XS_EXTRA_KEY = 0;
+int FISPACT_NUCLIDE_NAME_LENGTH = 0;
+int FISPACT_OUTPUT_DATA_INVENTORY_ALPHA_ACTIVITY = 0;
+int FISPACT_OUTPUT_DATA_INVENTORY_ALPHA_HEAT = 0;
+int FISPACT_OUTPUT_DATA_INVENTORY_BETA_ACTIVITY = 0;
+int FISPACT_OUTPUT_DATA_INVENTORY_BETA_HEAT = 0;
+int FISPACT_OUTPUT_DATA_INVENTORY_COOL_TIME = 0;
+int FISPACT_OUTPUT_DATA_INVENTORY_FLUX_AMP = 0;
+int FISPACT_OUTPUT_DATA_INVENTORY_GAMMA_ACTIVITY = 0;
+int FISPACT_OUTPUT_DATA_INVENTORY_GAMMA_HEAT = 0;
+int FISPACT_OUTPUT_DATA_INVENTORY_IRRAD_TIME = 0;
+int FISPACT_OUTPUT_DATA_INVENTORY_TOTAL_ACTIVITY = 0;
+int FISPACT_OUTPUT_DATA_INVENTORY_TOTAL_ATOMS = 0;
+int FISPACT_OUTPUT_DATA_INVENTORY_TOTAL_HEAT = 0;
+int FISPACT_OUTPUT_DATA_INVENTORY_TOTAL_MASS = 0;
+int FISPACT_VALID_GROUPS = 0;
+}
